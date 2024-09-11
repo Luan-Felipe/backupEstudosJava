46d325b532e05c006ad12b6c87f11e3fe48c657f
Luan Felipe <luanfel2009@gmail.com>
2024-09-11 13:25:37 -0300

    Relembrando uso de herança

---
+++ heranca/contas/src/application/Main.java
@@ -0,0 +1,10 @@
+package application;
+
+import entities.BusinessAcount;
+
+public class Main {
+    public static void main(String[] args) {
+        BusinessAcount ba = new BusinessAcount(123, "Luan Felipe", 5000.00, 5000.00);
+        
+    }
+}
